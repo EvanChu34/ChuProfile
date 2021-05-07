bed493d3479b138c915f8ed859bfebcea62f89b4
Evan Chu <echu34@gmail.com>
2021-05-06 22:06:52 -0400

    adding in js files and some code

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Container from "react-bootstrap/Container";
+
+
+function NavBar() {
+
+
+
+}
+
+export default NavBar;
---
+++ src/components/Particle.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Particles from "react-particles-js";
+
+function Particle() {
+
+
+}
+
+export default Particle;
